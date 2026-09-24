69f049f298db2796bb31182a7f60c1929c17e4af
santoshpawar02 <79916335+santoshpawar02@users.noreply.github.com>
2026-09-24 18:14:11 +0530

    service check

--- Lab-shell/catalogue.sh
+++ Lab-shell/catalogue.sh
@@ -2,6 +2,7 @@ dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
 cp -r catalogue.service /etc/systemd/system/catalogue.service
+cp -r mongo.repo /etc/yum.repos.d/mongo.repo
 useradd roboshop
 rm -rf /app 
 mkdir /app 
@@ -10,7 +11,6 @@ cd /app
 unzip /tmp/catalogue.zip
 cd /app 
 npm install 
-cp -r mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y
 systemctl daemon-reload
 mongosh --host mongo-dev.santoshwork.shop </app/db/master-data.js
